c0de8f1c9d61482091a9edf02563f8e358a36a14
Sky Ao <aoxiaojian@gmail.com>
2021-01-21 00:37:52 +0800

    update script

--- update_theme.sh
+++ update_theme.sh
@@ -6,14 +6,9 @@ if [ ! -d "themes" ];then
 fi
 
 if [ -d "themes/docsy" ];then
-  echo 'Find directoy "themes/docsy", update by "git pull"'
-  cd themes/docsy
-  git pull
-  cd ../../
+  echo 'Find directoy "themes/docsy"'
 else
   echo 'Directoy "themes/docsy" not found, create soft link'
-  #git clone https://github.com/skyao/docsy.git themes/docsy
-  #git clone git@github.com:skyao/docsy.git themes/docsy
   cd themes
   ln -s ../../docsy .
 fi
